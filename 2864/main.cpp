a3f5a64eff8cc401dfa981c53092b3c8d4cb9eca
choisw16 <choisw16@gmail.com>
2023-02-26 17:07:37 +0900

    0226 2864번

---
+++ 2864/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int max,min;
+
+    string a,b;
+    cin>>a>>b;
+    for(int i=0;i<a.size();i++){
+        if(a[i]=='6'){
+            a[i]='5';
+        }
+    }
+    for(int i=0;i<b.size();i++){
+        if(b[i]=='6'){
+            b[i]='5';
+        }
+    }
+    min = stoi(a)+ stoi(b);
+
+    for(int i=0;i<a.size();i++){
+        if(a[i]=='5'){
+            a[i]='6';
+        }
+    }
+    for(int i=0;i<b.size();i++){
+        if(b[i]=='5'){
+            b[i]='6';
+        }
+    }
+    max = stoi(a)+stoi(b);
+
+    cout<<min<<" "<<max<<endl;
+
+    return 0;
+}
