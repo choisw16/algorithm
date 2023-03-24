6b0bbf468f1bd338179d0a77e54f59f7ac81101b
choisw16 <choisw16@gmail.com>
2023-03-25 08:55:33 +0900

    0325 18258번

---
+++ 18258/main.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int N, x;
+string s;
+queue<int> q;
+
+int main()
+{
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> s;
+
+        if (s == "push")
+        {
+            cin >> x;
+            q.push(x);
+        }
+        else if (s == "pop")
+        {
+            if (!q.empty())
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+            else cout << -1 << '\n';
+
+        }
+        else if (s == "size")
+        {
+            cout << q.size() << '\n';
+        }
+        else if (s == "empty")
+        {
+            cout << q.empty() << '\n';
+        }
+        else if (s == "front")
+        {
+            if (!q.empty()) cout << q.front() << '\n';
+            else cout << -1 << '\n';
+        }
+        else
+        {
+            if (!q.empty()) cout << q.back() << '\n';
+            else cout << -1 << '\n';
+        }
+    }
+
+}
